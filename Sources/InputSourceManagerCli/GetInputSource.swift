e7a6b8a34acfc56d1cc8a3c81b6f07bc31e07e55
Robin Lieb <robin.j.lieb@gmail.com>
2022-04-15 09:19:18 +0200

    feat: added flag if keyboard layout input source should be provided

--- Sources/InputSourceManagerCli/GetInputSource.swift
+++ Sources/InputSourceManagerCli/GetInputSource.swift
@@ -9,19 +9,24 @@ struct GetInputSource: ParsableCommand {
         abstract: "Gets the current input source"
     )
     
+    @Flag(help: "Indicatetes if keyboard layout input source should be provided.")
+    var layout = false
+    
     func run() throws {
         let manager = InputSourceManager()
-        guard let inputSource = manager.getCurrentKeybaordInputSource() else {
-            print("No input source found")
-            return
-        }
         
-        guard let layouInputSource = manager.getCurrentKeybaordLayoutInputSource() else {
-            print("No input source found")
-            return
+        if layout {
+            guard let inputSource = manager.getCurrentKeybaordLayoutInputSource() else {
+                print("No input source found")
+                return
+            }
+            print("Id :\(inputSource.id), name: \(inputSource.localizedName)")
+        } else {
+            guard let inputSource = manager.getCurrentKeybaordInputSource() else {
+                print("No input source found")
+                return
+            }
+            print("Id :\(inputSource.id), name: \(inputSource.localizedName)")
         }
-        
-        print("Id :\(inputSource.id), name: \(inputSource.localizedName)")
-        print("Id :\(layouInputSource.id), name: \(layouInputSource.localizedName)")
     }
 }
